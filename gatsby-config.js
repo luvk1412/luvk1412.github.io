899886b98b8f3bbd1ef8a2756a93553908424e3b
luv <luvk1412@gmail.com>
2020-10-18 20:25:32 +0530

    added ga analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,5 +87,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-180798164-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
   ],
 };
